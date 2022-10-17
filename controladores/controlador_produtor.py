a9149c15ef913d90ba36ca8151d9f913e56d6430
guicardosooliveira <guicardosodeoliveira@gmail.com>
2022-10-17 20:54:52 -0300

    começo classe ControladorProdutor

---
+++ controladores/controlador_produtor.py
@@ -0,0 +1,32 @@
+from entidades.produtor import Produtor
+
+class ControladorProdutor:
+    def __init__(self):
+        self.__produtores = []
+
+    def inclui_produtor(self):
+        pass
+
+    def altera_produtor(self):
+        pass
+
+    def exclui_produtor(self):
+        pass
+
+    def listar_produtores(self):
+        pass
+
+    def adicionar_evento(self):
+        pass
+
+    def adicionar_venda_historico_vendas(self):
+        pass
+
+    def adicionar_evento_historico_eventos(self):
+        pass
+
+    def adicionar_evento_disponivel(self):
+        pass
+
+    def remover_evento_disponivel(self):
+        pass
