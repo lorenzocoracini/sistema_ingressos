fc2bfd935ca742fc18290098d5251af91623e7cb
lorenzocoracini <lorenzo291909@gmail.com>
2022-10-17 21:30:10 -0300

    criacao das classes telaevento e telaingresso

---
+++ telas/tela_evento.py
@@ -0,0 +1,9 @@
+class TelaEvento:
+    def mostar_opcoes(self):
+        pass
+
+    def pegar_dados(self):
+        pass
+
+    def listar_dados_eventos(self):
+        pass
---
+++ telas/tela_ingresso.py
@@ -0,0 +1,9 @@
+class TelaIngresso:
+    def mostar_opcoes(self):
+        pass
+
+    def pegar_dados(self):
+        pass
+
+    def listar_dados_eventos(self):
+        pass
