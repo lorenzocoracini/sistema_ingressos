5d1db58b88a15ac05286bac522d8b8a866d04f86
lorenzocoracini <lorenzo291909@gmail.com>
2022-10-24 19:58:09 -0300

    ajuste na opcao de voltar atraves de while true

--- controladores/controlador_produtor.py
+++ controladores/controlador_produtor.py
@@ -1,3 +1,4 @@
+from controladores.controlador_principal import ControladorPrincipal
 from entidades.produtor import Produtor
 from telas.tela_produtor import TelaProdutor
 
@@ -6,6 +7,7 @@ class ControladorProdutor:
     def __init__(self):
         self.__produtores = []
         self.__tela_produtor = TelaProdutor()
+        self.__tela_aberta = False
 
     def inclui_produtor(self, nome, cpf, nascimento, email, celular, senha):
         produtor = Produtor(nome, cpf, nascimento, email, celular, senha)
@@ -19,12 +21,22 @@ class ControladorProdutor:
         except SystemError:
             self.__tela_produtor.usuario_ja_existe()
 
+    def escolher_acao(self):
+        self.__tela_aberta = True
+        opcoes = {1: self.adicionar_evento, 2: self.editar_evento,
+                  3: self.transferir_ingresso, 4: self.alterar_produtor,
+                  5: self.exclui_produtor, 6: self.sair_da_conta}
+        while self.__tela_aberta:
+            opcao = self.__tela_produtor.mostra_tela_opcoes_pos_login()
+            opcoes[opcao]()
+
+
     def retorna_produtor_pelo_cpf(self, cpf):
         for produtor in self.__produtores:
             if produtor.cpf == cpf:
                 return produtor
 
-    def altera_produtor(self):
+    def alterar_produtor(self):
         pass
 
     def exclui_produtor(self):
@@ -39,6 +51,9 @@ class ControladorProdutor:
     def adicionar_venda_historico_vendas(self):
         pass
 
+    def editar_evento(self):
+        pass
+
     def adicionar_evento_historico_eventos(self):
         pass
 
@@ -50,3 +65,9 @@ class ControladorProdutor:
 
     def excluir_conta(self):
         pass
+
+    def transferir_ingresso(self):
+        pass
+
+    def sair_da_conta(self):
+        self.__tela_aberta = False
