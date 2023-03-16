e518f7849a2218339b96ce51fc84774686a3c980
Lorenzo Coracini <71295358+lorenzocoracini@users.noreply.github.com>
2023-03-16 19:09:42 -0300

    Update controlador_comprador.py
    Versão final

--- controladores/controlador_comprador.py
+++ controladores/controlador_comprador.py
@@ -1,100 +1,100 @@
 from entidades.comprador import Comprador
 from telas.tela_comprador import TelaComprador
+from exceptions.cpfEmUsoException import CpfEmUsoException
 
 
 class ControladorComprador:
-    def __init__(self, controlador_principal):
-        self.__tela_comprador = TelaComprador(self)
-        self.__compradores = []
+    def __init__(self, controlador_principal, controlador_evento, controlador_ingressos):
         self.__controlador_principal = controlador_principal
-        self.__tela_aberta = False
+        self.__controlador_evento = controlador_evento
+        self.__controlador_ingressos = controlador_ingressos
+        self.__compradores = []
+        self.__tela_comprador = TelaComprador()
 
-    def inclui_comprador(self, nome, cpf, email, celular, senha):
+    @property
+    def compradores(self):
+        return self.__compradores
+
+    def inclui_comprador(self, values):
+        nome = values['input_nome']
+        cpf = values['input_cpf']
+        email = values['input_email']
+        celular = values['input_celular']
+        senha = values['input_senha']
+
         comprador = Comprador(nome, cpf, email, celular, senha)
+
         try:
             for i in self.__compradores:
                 if comprador.cpf == i.cpf:
-                    raise SystemError
+                    raise CpfEmUsoException()
             else:
                 self.__compradores.append(comprador)
+                self.__controlador_principal.altera_usuario_logado(comprador)
+                self.mostrar_opcoes_comprador()
+
                 return comprador
-                #fazer metodo acao realizada com sucesso
-        except SystemError:
-            self.__tela_comprador.usuario_ja_existe()
+        except CpfEmUsoException:
+            self.__tela_comprador.mostra_mensagem("O cpf fornecido está vinculado a outra conta. Tente Novamente.")
 
-    def comprador_ja_existe(self):
-        self.__controlador_principal.inicia()
+    def mostrar_opcoes_comprador(self):
+        button, values = self.__tela_comprador.mostrar_opcoes()
+        opcoes = {'Ver meus ingressos': self.ver_meus_ingressos,
+                  'Ver eventos disponíveis': self.ver_eventos_disponiveis,
+                  'Comprar ingresso': self.comprar_ingresso,
+                  'Excluir conta': self.excluir_comprador,
+                  'Sair da conta': self.sair_da_conta}
+        opcoes[button]()
 
-    def retorna_comprador_e_senha_pelo_cpf(self, cpf):
-        for comprador in self.__compradores:
-            if comprador.cpf == cpf:
-                return [comprador, comprador.senha]
+    def comprar_ingresso(self):
+        deu_certo = False
+        self.__controlador_evento.listar_eventos()
+        while not deu_certo:
+            button, codigo_evento = self.__tela_comprador.pega_dados_para_compra_ingresso()
+            if button is None and codigo_evento is None:
+                self.__tela_comprador.mostra_mensagem("O formato de código inserido não é válido. "
+                                                      "Insira um número válido e tente novamente.")
+            elif button == "Cancel":
+                self.mostrar_opcoes_comprador()
+            else:
+                deu_certo = True
+                ingresso = None
+                for evento in self.__controlador_evento.eventos:
+                    if codigo_evento["input_codigo_evento"] == evento.codigo:
+                        if len(evento.ingressos) > 0:
+                            ingresso = evento.ingressos[0]
+                            evento.ingressos.remove(ingresso)
+                            evento.ingressos_vendidos.append(ingresso)
+                            self.__controlador_principal.usuario_logado.meus_ingressos.append(ingresso)
+                            self.__tela_comprador.mostra_mensagem("Ingresso comprado com sucesso!")
+                            self.mostrar_opcoes_comprador()
 
-    def escolher_acao(self):
-        self.__tela_aberta = True
-        opcoes = {1: self.ver_meus_ingressos, 2: self.ver_eventos_disponiveis,3: self.ver_eventos_favoritos,
-                  4: self.favoritar_evento, 5: self.remover_evento_favoritos, 6: self.comprar_ingresso,
-                  7: self.excluir_comprador, 8: self.sair_da_conta}
-        while self.__tela_aberta:
-            opcao = self.__tela_comprador.mostrar_opcoes_comprador()
-            print(opcao)
-            opcoes[opcao]()
+                        else:
+                            self.__tela_comprador.mostra_mensagem("Os ingressos para o evento fornecido estão esgotados")
+                            self.mostrar_opcoes_comprador()
+                if not ingresso:
+                    self.__tela_comprador.mostra_mensagem("O código de evento fornecido não existe")
+                    self.mostrar_opcoes_comprador()
+
+    def ver_meus_ingressos(self):
+        self.__controlador_ingressos.listar_ingressos()
+        self.mostrar_opcoes_comprador()
+
+    def ver_eventos_disponiveis(self):
+        self.__controlador_evento.listar_eventos()
+        self.mostrar_opcoes_comprador()
 
     def excluir_comprador(self):
         usuario_para_excluir = self.__controlador_principal.usuario_logado
         self.__compradores.remove(usuario_para_excluir)
         self.sair_da_conta()
 
-    def ver_meus_ingressos(self):
-        self.__tela_comprador.mostrar_meus_ingressos(self.__controlador_principal.usuario_logado.meus_ingressos)
-
-    def ver_eventos_disponiveis(self):
-        self.__controlador_principal.atualizar_eventos_disponiveis()
-        self.__tela_comprador.mostrar_eventos_disponiveis(self.__controlador_principal.eventos_disponiveis)
-
-    def ver_eventos_favoritos(self):
-        self.__tela_comprador.mostrar_eventos_favoritos(self.__controlador_principal.usuario_logado.eventos_favoritos)
-
-    def favoritar_evento(self):
-        codigo_evento_para_favoritar = self.__tela_comprador.pegar_dados_para_favoritar_evento()
-        evento_para_favoritar = None
-        for evento in self.__controlador_principal.eventos_disponiveis:
-            if evento.codigo == codigo_evento_para_favoritar:
-                evento_para_favoritar = evento
-        if evento_para_favoritar:
-            self.__controlador_principal.usuario_logado.eventos_favoritos.append(evento_para_favoritar)
-        else:
-            self.__tela_comprador.evento_nao_existe()
-
     def sair_da_conta(self):
-        self.__tela_aberta = False
-        self.__controlador_principal.deslogar()
+        self.__controlador_principal.altera_usuario_logado(None)
+        self.__controlador_principal.inicializa_sistema()
 
-    def comprar_ingresso(self):
-        self.ver_eventos_disponiveis()
-        nome_evento = self.__tela_comprador.pegar_evento_para_compra()
-        ingresso = None
-        for evento in self.__controlador_principal.eventos_disponiveis:
-            if nome_evento == evento.nome:
-                ingresso = evento.ingressos[0]
-                evento.ingressos.remove(ingresso)
-                evento.ingressos_vendidos.append(ingresso)
-                self.__controlador_principal.usuario_logado.meus_ingressos.append(ingresso)
-        if not ingresso:
-            self.__tela_comprador.evento_nao_existe()
-
-    def remover_evento_favoritos(self):
-        codigo_para_remover = self.__tela_comprador.pega_evento_remover_favoritos()
-        evento_para_ser_removido = None
-        for evento in self.__controlador_principal.usuario_logado.eventos_favoritos:
-            if evento.codigo == codigo_para_remover:
-                evento_para_ser_removido = evento
-        if evento_para_ser_removido:
-            self.__controlador_principal.usuario_logado.eventos_favoritos.remove(evento_para_ser_removido)
-        else:
-            self.__tela_comprador.evento_nao_existe()
-
-    @property
-    def compradores(self):
-        return self.__compradores
-
+    def retorna_comprador_pelo_cpf(self, cpf):
+        for comprador in self.__compradores:
+            if comprador.cpf == cpf:
+                return comprador
+        return None
