20e901344252ce67e3d3bb1cd80377891279c258
Lorenzo Coracini <71295358+lorenzocoracini@users.noreply.github.com>
2023-03-16 19:15:51 -0300

    Update produtor.py
    Versão final

--- entidades/produtor.py
+++ entidades/produtor.py
@@ -2,11 +2,10 @@ from entidades.usuario import Usuario
 
 
 class Produtor(Usuario):
-    def __init__(self, nome: str, cpf: str, email: str, celular: int, senha: str):
+    def __init__(self, nome: str, cpf: int, email: str, celular: int, senha: str):
         super().__init__(nome, cpf, email, celular, senha)
         self.__historico_eventos = []
 
     @property
     def historico_eventos(self):
         return self.__historico_eventos
-
